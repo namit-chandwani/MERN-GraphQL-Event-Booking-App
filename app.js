5feae4c88b0684dc3c780080e44443cf0d3f3805
Namit Chandwani <namitchandwani99@gmail.com>
2020-11-06 04:17:14 +0530

    Add GraphQL API endpoint to app.js

--- app.js
+++ app.js
@@ -1,9 +1,18 @@
 const express = require("express");
+const { graphqlHTTP } = require("express-graphql");
+
+const schema = require("./graphql/schema/schema");
 
 const app = express();
 
 const PORT = 5000;
 
-app.get("/", (req, res) => res.send("Hello World!"));
+app.use(
+  "/graphql",
+  graphqlHTTP({
+    graphiql: true,
+    schema,
+  })
+);
 
 app.listen(PORT, () => console.log(`Server listening on port: ${PORT}`));
