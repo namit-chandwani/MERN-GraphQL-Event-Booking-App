eaf6b5d17a5a31f8b3bf3f39c6a19ee35a5298bf
Namit Chandwani <namitchandwani99@gmail.com>
2020-11-11 06:09:26 +0530

    Add MongoDB connection

--- app.js
+++ app.js
@@ -1,11 +1,13 @@
 const express = require("express");
+const mongoose = require("mongoose");
 const { graphqlHTTP } = require("express-graphql");
 
 const schema = require("./graphql/schema/schema");
 
 const app = express();
+require("dotenv").config();
 
-const PORT = 5000;
+const PORT = 5000 || process.env.PORT;
 
 app.use(
   "/graphql",
@@ -15,4 +17,14 @@ app.use(
   })
 );
 
-app.listen(PORT, () => console.log(`Server listening on port: ${PORT}`));
+mongoose
+  .connect(process.env.MONGO_URI, {
+    useNewUrlParser: true,
+    useCreateIndex: true,
+    useUnifiedTopology: true,
+  })
+  .then(() => {
+    console.log("Connected to MongoDB database!");
+    app.listen(PORT, () => console.log(`Server listening on port: ${PORT}`));
+  })
+  .catch((err) => console.log(err));
