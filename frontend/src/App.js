eb1cbb96f4ab954a909ac4ad051496c0f04b5d00
Namit Chandwani <namitchandwani99@gmail.com>
2020-11-23 03:57:03 +0530

    Re-initialize frontend

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,23 +1,25 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import { Switch, Route, Redirect, BrowserRouter } from "react-router-dom";
+import Login from "./components/Login";
+import Register from "./components/Register";
+import Events from "./components/Events";
+import Bookings from "./components/Bookings";
+import Navbar from "./components/Navbar";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <BrowserRouter>
+        <Navbar />
+        <Switch>
+          {/* <Route path="/" component={}></Route> */}
+          <Redirect exact from="/" to="/login" />
+          <Route path="/login" component={Login}></Route>
+          <Route path="/register" component={Register}></Route>
+          <Route path="/events" component={Events}></Route>
+          <Route path="/bookings" component={Bookings}></Route>
+        </Switch>
+      </BrowserRouter>
     </div>
   );
 }
