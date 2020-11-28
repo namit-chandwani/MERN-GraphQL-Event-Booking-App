6b5a0b87737a37478c155ae8a7cd46360259a129
Namit Chandwani <namitchandwani99@gmail.com>
2020-11-28 09:10:53 +0530

    Fix Port number in app.js

--- app.js
+++ app.js
@@ -10,7 +10,7 @@ const isAuth = require("./middlewares/isAuth");
 const app = express();
 require("dotenv").config();
 
-const PORT = 5000 || process.env.PORT;
+const PORT = process.env.PORT || 5000;
 
 app.use(cors());
 app.use(isAuth);
